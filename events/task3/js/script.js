b3c384e923e734dd59bcc74f8f7578602305afbe
cm-ryffel <dimanlancev0@gmail.com>
2022-04-29 14:04:04 +0300

    добавил брейки после каждого кейса и исправил написание wich

--- events/task3/js/script.js
+++ events/task3/js/script.js
@@ -3,16 +3,19 @@ const second = document.querySelector('#second')
 const third = document.querySelector('#third')
 
 document.addEventListener('mousedown', function(e){
-    switch(e.witch){
+    switch(e.wich){
         case 1:
             first.style.left = e.pageX + "px";
             first.style.top = e.pageY + "px";
+            break
         case 2:
             second.style.left = e.pageX + "px";
             second.style.top = e.pageY + "px";
+            break
         case 3:
             third.style.left = e.pageX + "px";
             third.style.top = e.pageY + "px";
+            break
     }
 
 });
